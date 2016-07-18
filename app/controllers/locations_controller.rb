85e5851c26286f976f9a74bfb88f504f0d8319a9
root <root@hack-r.net>
2016-07-18 12:42:30 -0500

    Added/Finalized homepage while adding a page for Locations

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,48 @@
+class LocationsController < ApplicationController
+   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(params.require(:location).permit(:region, :institution))
+    if @location.save
+      redirect_to @location
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    if @location.update(params.require(:tag).permit(:keyword))
+      redirect_to @location
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @locations = Location.all
+  end
+  
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+    
+    redirect_to locations_path
+  end
+  private 
+  def locations_params
+    params.require(:location).permit(:region, :institutiony)
+  end
+end
+
